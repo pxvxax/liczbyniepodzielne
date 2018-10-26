7857e04dc7f661b59510e4d4ad2557f4e92b4b79
pxvxax <pxvxax@gmail.com>
2018-10-26 09:20:57 +0200

    xxx lekka zmiana wprowadzenie liczby

--- main.c
+++ main.c
@@ -5,10 +5,13 @@ int main() {
     int licznik_petli=0;
 
 
-    for (a = 999; !(a%3) || !(a%5) || !(a%7); a -= 2) {
+    printf("Podaj a: "); scanf("%d", &a);
+    if (!(a % 2)) a--;
+
+    for (; !(a%3) || !(a%5) || !(a%7); a -= 2) {
         licznik_petli++;
     }
-    printf("znaleziona liczba: %d\n", a);
+    printf("znaleziona liczba to: %d\n", a);
 
     printf("\n");
     printf("liczba pętli wyniosła: %d\n", licznik_petli);
